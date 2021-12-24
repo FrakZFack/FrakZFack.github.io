6ab13e6b85fe7cd2c051667c51749b1cc199d5b7
FrakZFack <96567139+FrakZFack@users.noreply.github.com>
2021-12-24 14:57:00 +0700

    Add files via upload

---
+++ php/help.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css2?family=Cuprum&display=swap" rel="stylesheet">
+ <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ПОДДЕРЖКА</title>
+  
+</head>
+<body>
+  
+  <nav>
+     <a class="eco" href="index.html">ECO-SPORT</a>
+    <a href="catalog.my.html">Каталог</a>
+    <a href="contacts.html">Контакты</a>
+    <a href="help.html">Поддержка</a>
+    <a href="oNas.html">О нас</a>
+    <a href="#">Корзина</a>
+  <span>МАГАЗИН СПОРТИВНОЙ ЭКИПИРОВКИ</span>
+  </nav>
+  <main>
+
+    
+
+    <article class="helps">
+      <div class="wrapper">
+        <input type="checkbox" id="modal">
+        <label class="btn-modal" for="modal">Задать вопрос</label>
+<form action="php/send.php" method="post" class="popup">
+  <p>Заполните поля</p>
+  <input name="name_user" placeholder="Ваше имя" type="text" required="">
+  <input name="mail_user" placeholder="Ваш электронный адрес" type="text" required="">
+  <input name="report_user" placeholder="Ваш вопрос / жалоба" type="text" required="">
+  <input class="submit" type="submit" value="отправить">
+<label class="btn-close" for="modal">+</label>
+</form>
+
+<img src="img/help.png" alt="help">
+      </div>
+        
+      
+    
+      
+    
+    
+    </article>
+
+    
+       
+       
+       
+    
+        
+     </article>
+  </main>
+  <footer>
+    <ul>
+      <a href="index.html"><li>Главная</li></a>
+      <a href="catalog.my.html"><li>Каталог</li></a>
+     <a href="help.html"><li>Поддержка</li></a>
+      <a href="contacts.html"><li>Контакты</li></a>
+    </ul>
+    <ul>
+      <li>Акция</li>
+      <li>Условия акции</li>
+      <li>Время работы: 9:00 - 21:00</li>
+      <li>© ECO-SPORT 2019-2021</li>
+       
+
+    </ul>
+    <ul>
+      <li>8-800-555-35-35</li>
+      <li>sobakamail@mail.ru</li>
+      <li>Адрес - г.Кемерово</li>
+      <li>ул.Рукавишникова 228/1337</li>
+    </ul>
+  </footer>
+</body>
+</html>
---
+++ php/send.php
@@ -0,0 +1,38 @@
+﻿<?php 
+$name_user = $_POST['name_user'];
+$mail_user = $_POST['mail_user'];
+$report_user = $_POST['report_user'];
+
+
+
+
+
+$name_user = htmlspecialchars($name_user);
+$mail_user = htmlspecialchars($mail_user);
+$report_user = htmlspecialchars($report_user);
+
+$name_user = urldecode($name_user);
+$mail_user = urldecode($mail_user);
+$report_user = urldecode($report_user);
+
+$name_user = trim($name_user);
+$mail_user = trim($mail_user);
+$report_user = trim($report_user);
+
+
+if(mail($mail_user,
+	"Новое письмо с сайта",
+	"Имя клиента: ".$name_user."\n".
+	"Почтовый ящик клиента: ".$mail_user."\n".
+	"Вопрос/Жалоба: ".$report_user."\n",
+	"From: <eco_sport@list.ru> \r\n")
+)   {
+	echo ('Письмо успешно отправлено');
+}
+else {
+	echo ('Есть ошибки! Проверьте данные...');
+}
+
+
+
+ ?>
